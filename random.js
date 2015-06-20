0643ad91b76d0a6b6c5e0a49d1d6170dc1dc003e
git-user-bcn <david.monne@yahoo.es>
2015-06-20 17:47:12 +0100

    random versión 1

---
+++ random.js
@@ -0,0 +1,11 @@
+// Math.random() devuelve un número aleatorio entre 0 y 1
+var numero = Math.randmo();
+
+if (numero <= 0.5)
+{
+  console.log('\n' + numero + ' MENOR que 0,5 \n');
+}
+else
+{
+  console.log('\n' + numero + ' MAYOR que 0,5 \n');
+}
